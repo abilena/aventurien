5ccd2b11419fed14cbf14b16bee70c587c02fc40
wallnerk <klemens.wallner@avl.com>
2016-11-21 22:03:16 +0100

    Removed old theme tryout

--- leftbar.php
+++
@@ -1,8 +0,0 @@
-<div class="aventurien-bar-left-upper">
-    <div class="sidebar-module">
-        <h4>Left</h4>
-        <ol class="list-unstyled">
-	        <?php wp_get_archives( 'type=monthly' ); ?>
-        </ol>
-    </div>
-</div><!-- /.aventurien-bar-left-upper -->
--- rightbar.php
+++
@@ -1,8 +0,0 @@
-<div class="aventurien-bar-right-upper">
-    <div class="sidebar-module">
-        <h4>Right</h4>
-        <ol class="list-unstyled">
-	        <?php wp_get_archives( 'type=monthly' ); ?>
-        </ol>
-    </div>
-</div><!-- /.aventurien-bar-right-upper -->
